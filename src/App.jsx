691b07a4d3efeb2de6377e7383f0b2b321cf8e52
CYJ_Manager <manager@cyj.com>
2026-06-08 12:09:38 +0800

    3.2.1

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ import {
 // ==========================================
 // ★ 系統核心版本號 (終極動態快取版)
 // ==========================================
-const CURRENT_APP_VERSION = "3.2.0"; 
+const CURRENT_APP_VERSION = "3.2.1"; 
 const LOGIN_LOCATION_ENDPOINT = "https://resolveloginlocation-hyhcwrnyaa-uc.a.run.app";
 
 
